e7e80c5a4f0b87d7a7c13014109a8c6b5f5476c6
romain-poms <you@example.com>
2020-03-29 13:10:52 +0200

    update

--- src/mixins/fetchers.js
+++ src/mixins/fetchers.js
@@ -1,8 +1,12 @@
 export default {
+    computed: {
+        apiKey() {
+            return this.$store.state.apiKey
+        },
+    },
     methods: {
         getData: async function(mode, query) {
-            let apiKey = "a0cad519d1cc21ef19d3f29bbc58c5d0"
-            let response = await fetch("https://api.themoviedb.org/3/" + mode + "?api_key=" + apiKey + "&language=fr" + query);
+            let response = await fetch("https://api.themoviedb.org/3/" + mode + "?api_key=" + this.apiKey + "&language=fr" + query);
             let data = await response.json()
             return data;
         },
